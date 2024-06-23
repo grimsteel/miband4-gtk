8af2b9d65297e9480c71fb298456b422ed6ece24
Siddhant Kameswar <115331356+grimsteel@users.noreply.github.com>
2024-06-23 15:24:04 -0500

    Restructure/organize code

---
+++ src/band.rs
@@ -0,0 +1,234 @@
+use std::{collections::{HashMap, HashSet}, error::Error, fmt::Display, time::Duration};
+
+use bluer::{gatt::remote::{Service, Characteristic}, id::{Characteristic as CharId, Service as ServiceId}, Adapter, AdapterEvent, Address, Device, DiscoveryFilter, DiscoveryTransport, Uuid};
+use chrono::{DateTime, Datelike, Local, TimeZone, Timelike};
+use futures::pin_mut;
+use tokio_stream::StreamExt;
+
+
+macro_rules! uuid {
+    ($var_name:ident,$uuid:expr) => {
+        const $var_name: Uuid = match Uuid::try_parse($uuid) {
+            Ok(u) => u,
+            _ => panic!("bad uuid")
+        };
+    }
+}
+
+uuid!(SERVICE_BAND_0, "0000fee0-0000-1000-8000-00805f9b34fb");
+uuid!(CHAR_BATTERY, "00000006-0000-3512-2118-0009af100700");
+uuid!(CHAR_STEPS, "00000007-0000-3512-2118-0009af100700");
+
+struct BandChars {
+    battery: Characteristic,
+    steps: Characteristic,
+    firm_rev: Characteristic,
+    time: Characteristic
+}
+
+#[derive(Debug)]
+pub enum BandError {
+    BluerError(bluer::Error),
+    MissingServicesOrChars,
+    NotInitialized,
+    InvalidTime
+}
+
+impl From<bluer::Error> for BandError {
+    fn from(value: bluer::Error) -> Self {
+        Self::BluerError(value)
+    }
+}
+
+impl Display for BandError {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "{:?}", self)
+    }
+}
+
+impl Error for BandError {
+    
+}
+
+type Result<T> = std::result::Result<T, BandError>;
+
+pub struct MiBand {
+    device: Device,
+    pub authenticated: bool,
+    chars: Option<BandChars>,
+    auth_key: Option<Vec<u8>>
+}
+
+#[derive(Debug)]
+pub struct BatteryStatus {
+    battery_level: u8,
+    last_off: DateTime<Local>,
+    last_charge: DateTime<Local>,
+    charging: bool
+}
+
+// parse a time out of a 7 byte array
+fn parse_time(value: &[u8]) -> Option<DateTime<Local>> {
+    if value.len() < 7 { return None }
+    
+    let year = (value[0] as u16) | ((value[1] as u16) << 8);
+    let month = value[2];
+    let day = value[3];
+    let hour = value[4];
+    let minute = value[5];
+    let second = value[6];
+
+    let time = Local.with_ymd_and_hms(year.into(), month.into(), day.into(), hour.into(), minute.into(), second.into());
+    match time {
+        chrono::offset::LocalResult::Single(time) => Some(time),
+        _ => None
+    }
+}
+
+fn time_to_bytes(time: &DateTime<Local>) -> Vec<u8> {
+    let year = time.year();
+    vec![(year & 0xff) as u8, (year >> 8) as u8, time.month() as u8, time.day() as u8, time.hour() as u8, time.minute() as u8, time.second() as u8]
+}
+
+// helper functions for getting all services/chars
+async fn get_all_services(device: &Device) -> Result<HashMap<Uuid, Service>> {
+    let mut map = HashMap::new();
+    for service in device.services().await? {
+        let uuid = service.uuid().await?;
+        map.insert(uuid, service);
+    }
+    Ok(map)
+}
+
+async fn get_all_chars(service: &Service) -> Result<HashMap<Uuid, Characteristic>> {
+    let mut map = HashMap::new();
+    for characteristic in service.characteristics().await? {
+        let uuid = characteristic.uuid().await?;
+        map.insert(uuid, characteristic);
+    }
+    Ok(map)
+}
+
+impl MiBand {
+    pub fn new(device: Device, auth_key: Option<Vec<u8>>) -> Self {
+        Self {
+            device,
+            auth_key,
+            authenticated: false,
+            chars: None
+        }
+    }
+
+    pub async fn is_connected(&self) -> bool {
+        self.device.is_connected().await.unwrap_or(false)
+    }
+
+    pub async fn initialize(&mut self) -> Result<()> {
+        // first connect if needed
+        let was_connected = self.is_connected().await;
+        if !was_connected {
+            self.device.connect().await?;
+        }
+
+        // if we weren't connected of if we don't have the chars, fetch them
+        if !was_connected || self.chars.is_none() {
+            self.fetch_chars().await?;
+        }
+
+        Ok(())
+    }
+
+    /// Note: device must be connected here
+    async fn fetch_chars(&mut self) -> Result<()> {
+        // get the services
+        let services = get_all_services(&self.device).await?;
+
+        match (services.get(&SERVICE_BAND_0), services.get(&ServiceId::DeviceInformation.into())) {
+            (Some(band_0), Some(device_info)) => {
+                let mut band_0 = get_all_chars(band_0).await?;
+                let mut device_info = get_all_chars(device_info).await?;
+                // get the characteristics from their respective services
+                match (band_0.remove(&CHAR_BATTERY), band_0.remove(&CHAR_STEPS), band_0.remove(&CharId::CurrentTime.into()), device_info.remove(&CharId::SoftwareRevisionString.into())) {
+                    (Some(battery), Some(steps), Some(time), Some(firm_rev)) => {
+                        let chars = BandChars {
+                            battery, steps, time, firm_rev
+                        };
+
+                        self.chars = Some(chars);
+
+                        return Ok(());
+                    },
+                    _ => {}
+                }
+            },
+            _ => {}
+        }
+
+        return Err(BandError::MissingServicesOrChars);
+    }
+
+    pub async fn get_battery(&self) -> Result<BatteryStatus> {
+        if let Some(BandChars { battery, .. }) = &self.chars {
+            let value = battery.read().await?;
+            let battery_level = value[1];
+            let charging = value[2] != 0;
+
+            let last_off = parse_time(&value[3..]).ok_or(BandError::InvalidTime)?;
+            let last_charge = parse_time(&value[11..]).ok_or(BandError::InvalidTime)?;
+
+            Ok(BatteryStatus {
+                battery_level,
+                charging,
+                last_off,
+                last_charge
+            })
+        } else { Err(BandError::NotInitialized) }
+    }
+    
+    pub async fn discover(adapter: &Adapter, timeout: Duration)  -> Result<HashMap<Address, Device>> {
+        let mut device_map = HashMap::new();
+        
+        if !adapter.is_powered().await? {
+            eprintln!("Adapter is not on");
+            return Ok(device_map)
+        }
+        if adapter.is_discovering().await? {
+            eprintln!("Adapter is already discovering");
+            return Ok(device_map)
+        }
+
+        // filter to just the mi band service
+        let filter = DiscoveryFilter {
+            uuids: HashSet::from([SERVICE_BAND_0]),
+            transport: DiscoveryTransport::Le,
+            duplicate_data: false,
+            discoverable: false,
+            ..Default::default()
+        };
+        
+        adapter.set_discovery_filter(filter).await?;
+        // start discovering
+        let devices = adapter.discover_devices().await?.timeout(timeout);
+        pin_mut!(devices);
+        while let Some(Ok(event)) = devices.next().await {
+            match event {
+                AdapterEvent::DeviceAdded(addr) => {
+                    if let Ok(device) = adapter.device(addr.clone()) {
+                        // limit to devices that are actually present
+                        // add it to the map
+                        device_map.insert(addr, device);
+                        
+                        // temp helper:
+                        break;
+                    }
+                },
+                AdapterEvent::DeviceRemoved(addr) => {
+                    device_map.remove(&addr);
+                }
+                _ => {}
+            }
+        }
+        
+        Ok(device_map)
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,164 +1,33 @@
-use std::{collections::{HashMap, HashSet}, error::Error, time::Duration};
+use std::{env, error::Error, str::FromStr, time::Duration};
 
-use bluer::{Adapter, AdapterEvent, Address, Device, DiscoveryFilter, DiscoveryTransport, Session, Uuid, id::{Characteristic, Service}};
-use chrono::{DateTime, Datelike, Local, TimeZone, Timelike};
-use futures::pin_mut;
-use tokio_stream::StreamExt;
+use band::MiBand;
+use bluer::{Address, Session};
 
-macro_rules! uuid {
-    ($var_name:ident,$uuid:expr) => {
-        const $var_name: Uuid = match Uuid::try_parse($uuid) {
-            Ok(u) => u,
-            _ => panic!("bad uuid")
-        };
-    }
-}
-
-uuid!(SERVICE_BAND_0, "0000fee0-0000-1000-8000-00805f9b34fb");
-uuid!(CHAR_BATTERY, "00000006-0000-3512-2118-0009af100700");
-uuid!(CHAR_STEPS, "00000007-0000-3512-2118-0009af100700");
-
-async fn discover_mi_bands(adapter: &Adapter, duration_ms: u64) -> bluer::Result<HashMap<Address, Device>> {
-    let mut device_map = HashMap::new();
-    
-    if !adapter.is_powered().await? {
-        eprintln!("Adapter is not on");
-        return Ok(device_map)
-    }
-    if adapter.is_discovering().await? {
-        eprintln!("Adapter is already discovering");
-        return Ok(device_map)
-    }
-
-    // filter to just the mi band service
-    let filter = DiscoveryFilter {
-        uuids: HashSet::from([SERVICE_BAND_0]),
-        transport: DiscoveryTransport::Le,
-        duplicate_data: false,
-        discoverable: false,
-        ..Default::default()
-    };
-    
-    adapter.set_discovery_filter(filter).await?;
-    // start discovering
-    let devices = adapter.discover_devices().await?.timeout(Duration::from_millis(duration_ms));
-    pin_mut!(devices);
-    while let Some(Ok(event)) = devices.next().await {
-        match event {
-            AdapterEvent::DeviceAdded(addr) => {
-                if let Ok(device) = adapter.device(addr.clone()) {
-                    // limit to devices that are actually present
-                    // add it to the map
-                    device_map.insert(addr, device);
-                    
-                    // temp helper:
-                    break;
-                }
-            },
-            AdapterEvent::DeviceRemoved(addr) => {
-                device_map.remove(&addr);
-            }
-            _ => {}
-        }
-    }
-    
-    Ok(device_map)
-}
-
-// parse a time out of a 7 byte array
-fn parse_time(value: &[u8]) -> Option<DateTime<Local>> {
-    if value.len() < 7 { return None }
-    
-    let year = (value[0] as u16) | ((value[1] as u16) << 8);
-    let month = value[2];
-    let day = value[3];
-    let hour = value[4];
-    let minute = value[5];
-    let second = value[6];
-
-    let time = Local.with_ymd_and_hms(year.into(), month.into(), day.into(), hour.into(), minute.into(), second.into());
-    match time {
-        chrono::offset::LocalResult::Single(time) => Some(time),
-        _ => None
-    }
-}
-
-fn time_to_bytes(time: &DateTime<Local>) -> Vec<u8> {
-    let year = time.year();
-    vec![(year & 0xff) as u8, (year >> 8) as u8, time.month() as u8, time.day() as u8, time.hour() as u8, time.minute() as u8, time.second() as u8]
-}
+mod band;
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn Error>> {
     let session = Session::new().await?;
     // get the adapter
     let adapter = session.default_adapter().await?;
-    let found = discover_mi_bands(&adapter, 50000).await?;
+
+    let found = MiBand::discover(&adapter, Duration::from_secs(50)).await?;
+    let device = found.into_values().next().unwrap();
+
+    //let mac_address = env::var("BAND_MAC").unwrap();
+    //let device = adapter.device(Address::from_str(&mac_address).unwrap()).unwrap();
+
+    let mut band = MiBand::new(device, None);
+    band.initialize().await?;
+    println!("{:?}", band.get_battery().await);
+    /*let found = discover_mi_bands(&adapter, 50000).await?;
     let test_device = found.values().next().unwrap();
     println!("connecting to device {}", test_device.address());
     if !test_device.is_connected().await? {
         test_device.connect().await?;
     }
-    println!("connected successfully");
-
-    let services = test_device.services().await?;
-    let mut service_band_0 = None;
-    let mut service_device_info = None;
-    for service in services {
-        let uuid = service.uuid().await?;
-        if uuid == SERVICE_BAND_0 {
-            service_band_0 = Some(service);
-        } else if uuid == Service::DeviceInformation.into() {
-            service_device_info = Some(service);
-        }
-    }
-    for characteristic in service_band_0.unwrap().characteristics().await? {
-        let uuid = characteristic.uuid().await?;
-        if uuid == CHAR_BATTERY {
-            let value = characteristic.read().await?;
-            let battery_level = value[1];
-            let charging = value[2] != 0;
-
-            let last_off = parse_time(&value[3..]).unwrap().to_string();
-            let last_charge = parse_time(&value[11..]).unwrap().to_string();
-            
-            println!("Battery level: {battery_level}\nCharging: {charging}\nLast off: {last_off}\nLast charge: {last_charge}");
-        } else if uuid == Characteristic::CurrentTime.into() {
-            let value = characteristic.read().await?;
-            //println!("{:?}", value);
-            println!("time: {:?}", parse_time(&value));
-            println!("syncing current time");
-            let mut now = time_to_bytes(&Local::now());
-            let mut rest: Vec<u8> = vec![Local::now().weekday().num_days_from_sunday() as u8, 0, 0, 0];
-            now.append(&mut rest);
-            //println!("{:?}", now);
-            /*let req = CharacteristicWriteRequest {
-                offset: 0,
-                op_type: bluer::gatt::WriteOp::Request,
-                prepare_authorize: false,
-                ..Default::default()
-            };*/
-            // this doesn't work yet because we need to authenticate
-            // characteristic.write_ext(&now, &req).await?;
-        }/* this also requires auth
-        else if uuid == CHAR_STEPS {
-            let value = characteristic.read().await?;
-            let steps = (value[1] as u16) & ((value[2] as u16) << 8);
-            let meters = (value[5] as u16) & ((value[6] as u16) << 8);
-            let calories = (value[9] as u16) & ((value[10] as u16) << 8);
-            println!("Steps: {steps}, Meters: {meters}, Calories burned: {calories}");
-    }*/
-    }
-
-    for characteristic in service_device_info.unwrap().characteristics().await? {
-        let uuid = characteristic.uuid().await?;
-        let value = characteristic.read().await?;
-        if uuid == Characteristic::HardwareRevisionString.into() {
-            println!("Hardware revision: {}", String::from_utf8(value).unwrap());
-        } else if uuid == Characteristic::SoftwareRevisionString.into() {
-            println!("Software revision: {}", String::from_utf8(value).unwrap());
-        }
-    }
+    println!("connected successfully, authenticating");
+    //let auth_key = env::var("BAND_AUTH_KEY").unwrap();*/
 
     Ok(())
 }
