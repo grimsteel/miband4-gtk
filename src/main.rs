42cc41e50fbd5fce380255af2f1c1cace163b27e
Siddhant Kameswar <115331356+grimsteel@users.noreply.github.com>
2024-06-26 18:19:28 -0500

    styling, device row layout

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,5 @@
-use std::{env, time::Duration};
-
-use band::MiBand;
-use bluez::BluezSession;
-use gtk::{gio::resources_register_include, glib::{spawn_future_local, ExitCode}, prelude::*, Application};
+use gtk::{gdk::Display, gio::resources_register_include, glib::ExitCode, prelude::*, style_context_add_provider_for_display, Application, CssProvider, STYLE_PROVIDER_PRIORITY_APPLICATION};
 use ui::window::MiBandWindow;
-use utils::decode_hex;
 
 mod band;
 mod utils;
@@ -18,30 +13,19 @@ fn main() -> ExitCode {
     resources_register_include!("resources.gresource").expect("failed to register resources");
     
     let app = Application::builder().application_id(APP_ID).build();
+    app.connect_startup(|_app| {
+        let provider = CssProvider::new();
+        provider.load_from_resource("/me/grimsteel/miband4-gtk/style.css");
+        style_context_add_provider_for_display(
+            &Display::default().expect("Could not connect to display"),
+            &provider,
+            STYLE_PROVIDER_PRIORITY_APPLICATION,
+        );
+        
+    });
     // connect a handler to the activate signal
-    app.connect_activate(build_ui);
+    app.connect_activate(|app| {
+        MiBandWindow::new(app).present();
+    });
     app.run()
-}
-
-fn build_ui(app: &Application) {
-    let window = MiBandWindow::new(app);
-    window.present();
-
-    /*spawn_future_local(async move {
-        if let Ok(session) = BluezSession::new().await {
-            let powered = session.adapter.powered().await.unwrap_or(false);
-            button.set_visible(powered);
-            text_unpowered.set_visible(!powered);
-
-            if let Ok(all_bands) = MiBand::discover(session.clone(), Duration::from_secs(5)).await {
-                let band = all_bands.first().unwrap();
-                println!("found device {band:?}");
-                if let Ok(mut band) = MiBand::from_discovered_device(session.clone(), band).await {
-                    println!("{:?}", band.initialize().await);
-                    println!("{:?}", band.authenticate(&auth_key).await);
-                    
-                }
-            };
-        };
-    });*/
 }
