7a627b76802f424df4bc4447a6e131a432e062ec
Siddhant Kameswar <115331356+grimsteel@users.noreply.github.com>
2024-07-06 18:40:46 -0500

    finish up mpris

--- src/mpris.rs
+++ src/mpris.rs
@@ -35,19 +35,25 @@ pub struct MprisController<'a> {
     playerctl_proxy: PlayerCtlDProxy<'a>
 }
 
-#[derive(Debug)]
+#[derive(Debug, Copy, Clone)]
 pub enum MediaState {
     Playing,
     Paused,
     Stopped
 }
 
-#[derive(Debug)]
+impl Default for MediaState {
+    fn default() -> Self {
+        Self::Stopped
+    }
+}
+
+#[derive(Debug, Default, Clone)]
 pub struct MediaInfo {
     pub track: String,
-    pub volume: f64,
-    pub position: i64,
-    pub duration: i64,
+    pub volume: u8, // 0 to 100
+    pub position: u64,
+    pub duration: u64,
     pub state: MediaState
 }
 
@@ -64,31 +70,82 @@ impl<'a> MprisController<'a> {
     }
 
     pub async fn watch_changes(&self, mut tx: Sender<Option<MediaInfo>>) -> zbus::Result<()> {
-        // update the current player
-        //*self.current_player.write().await = self.get_first_player().await?;
-        let mut players_stream = self.playerctl_proxy.receive_player_names_changed().await.fuse();
+        // setup all of the streams
+        let players_stream = self.playerctl_proxy.receive_player_names_changed().await
+            .then(|e| async move {
+                let players =  e.get().await;
+                // return true if there's at least 1 player
+                players.map(|p| p.len() > 0).unwrap_or(false)
+            }).fuse();
+        pin_mut!(players_stream);
 
-        while let Some(players) = players_stream.next().await {
-            let players = players.get().await?;
-            if players.len() == 0 {
-                let _ = tx.send(None).await;
-            } else {
-                let metadata = self.player_proxy.metadata().await?;
-                let title = metadata.get("xesam:title").and_then(|s| s.downcast_ref().ok()).unwrap_or("Unknown title");
-                let duration_micros: i64 = metadata.get("mpris:length").and_then(|s| s.downcast_ref().ok()).unwrap_or_default();
-                // optional
-                let position_micros = self.player_proxy.position().await.unwrap_or_default();
-                // optional
-                let volume = self.player_proxy.volume().await.unwrap_or_default();
-                let state = self.player_proxy.playback_status().await?;
-                let state = match state.as_str() {
-                    "Playing" => MediaState::Playing,
-                    "Paused" => MediaState::Paused,
-                    _ => MediaState::Stopped
-                };
-                let item = MediaInfo { track: title.into(), volume, position: position_micros, duration: duration_micros, state };
-                let _ = tx.send(Some(item)).await;
-            }
+        // wait for at least one player to start before proceeding
+        while let Some(false) = players_stream.next().await {}
+        
+        let mut metadata_stream = self.player_proxy.receive_metadata_changed().await.fuse();
+        let mut playback_status_stream = self.player_proxy.receive_playback_status_changed().await.fuse();
+        let mut volume_stream = self.player_proxy.receive_volume_changed().await.fuse();
+        let mut position_stream = self.player_proxy.receive_position_changed().await.fuse();
+
+        let mut current_media_info = MediaInfo::default();
+        let mut players_exist = true;
+
+        loop {
+            select! {
+                new_players_exist = players_stream.next() => {
+                    players_exist = new_players_exist.unwrap_or_default();
+                },
+                metadata = metadata_stream.next() => {
+                    if let Some(metadata) = metadata {
+                        if let Ok(metadata) = metadata.get().await {
+                            let title = metadata.get("xesam:title").and_then(|s| s.downcast_ref().ok()).unwrap_or("Unknown title");
+                            let duration_micros: i64 = metadata.get("mpris:length").and_then(|s| s.downcast_ref().ok()).unwrap_or_default();
+                            current_media_info.track = title.into();
+                            current_media_info.duration = duration_micros.try_into().unwrap_or_default();
+                        } else {
+                            // set default values
+                            current_media_info.track = "".into();
+                            current_media_info.duration = 0;
+                        }
+                    }
+                },
+                pos = position_stream.next() => {
+                    if let Some(pos) = pos {
+                        let pos = pos.get().await.unwrap_or_default();
+                        current_media_info.position = pos.try_into().unwrap_or_default();
+                    }
+                },
+                volume = volume_stream.next() => {
+                    if let Some(volume) = volume {
+                        let volume = volume.get().await.unwrap_or_default();
+                        // add bounds to the volume and cast to u8
+                        let volume = (volume * 100f64).max(0f64).min(100f64) as u8;
+                        current_media_info.volume = volume;
+                    }
+                },
+                status = playback_status_stream.next() => {
+                    if let Some(status) = status {
+                        let status = status.get().await
+                            // transform  the status string to our enum
+                            .map(|s| match s.as_str() {
+                                "Playing" => MediaState::Playing,
+                                "Paused" => MediaState::Paused,
+                                _ => MediaState::Stopped
+                            }).unwrap_or_default();
+                        current_media_info.state = status;
+                    }
+                }
+            };
+
+            if tx.send(
+                if players_exist {
+                    Some(current_media_info.clone())
+                } else {
+                    None
+                }).await.is_err()
+            {
+                break
+            };
         }
 
         Ok(())
