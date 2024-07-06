2e116f3772385de756494e6d14c01b9af8a0f764
Siddhant Kameswar <115331356+grimsteel@users.noreply.github.com>
2024-07-06 14:40:27 -0500

    Continue mpris work

--- src/mpris.rs
+++ src/mpris.rs
@@ -1,6 +1,9 @@
 use std::sync::RwLock;
 
-use zbus::{fdo::DBusProxy, names::OwnedBusName, Connection, proxy};
+use zbus::{fdo::DBusProxy, names::{BusName, OwnedBusName, WellKnownName}, proxy, Connection};
+use futures::{stream::StreamExt, select, pin_mut};
+
+use log::debug;
 
 #[proxy(default_path = "/org/mpris/MediaPlayer2", interface = "org.mpris.MediaPlayer2.Player", gen_blocking = false)]
 trait MediaPlayer {
@@ -30,28 +33,83 @@ impl<'a> MprisController<'a> {
     pub async fn init() -> zbus::Result<Self> {
         let conn = Connection::session().await?;
         let dbus_proxy = DBusProxy::new(&conn).await?;
-        let current_player = Self::get_current_player(&conn, &dbus_proxy).await?;
         Ok(Self {
             conn,
             dbus: dbus_proxy,
-            current_player: RwLock::new(current_player)
+            current_player: RwLock::new(None)
         })
     }
 
-    async fn get_current_player<'b, 'c>(conn: &Connection, proxy: &DBusProxy<'b>) -> zbus::Result<Option<MediaPlayerProxy<'c>>> {
-        let bus_names: Vec<OwnedBusName> = proxy.list_activatable_names().await?;
+    async fn refresh_current_player<'b, 'c>(&self) -> zbus::Result<()> {
+        let bus_names: Vec<OwnedBusName> = self.dbus.list_names().await?;
 
+        let mut player = self.current_player.write().expect("can write to current player");
+        
         // For right now, just use the first one
-        if let Some(mpris_bus_name) = bus_names
+        *player = if let Some(mpris_bus_name) = bus_names
             .into_iter()
             .filter(|name| name.starts_with("org.mpris.MediaPlayer2"))
             .next()
         {
-            Ok(Some(MediaPlayerProxy::new(conn, mpris_bus_name).await?))
+            Some(MediaPlayerProxy::new(&self.conn, mpris_bus_name).await?)
         } else {
-            Ok(None)
-        }
+            None
+        };
+
+        Ok(())
     }
 
-    
+    async fn watch_changes(&self) -> zbus::Result<()> {
+        self.refresh_current_player().await?;
+        
+        loop {
+            let current_player = self.current_player.read().expect("can read player");
+            if let Some(player) = current_player.as_ref() {
+                // wait for this player to disappear from the bus
+                let player_name = player.0.destination().as_str();
+
+
+                // TODO: actually wait
+                debug!("Player {:?} disappeared", player_name);
+
+                // drop early so refresh_current_player can write
+                drop(current_player);
+                self.refresh_current_player().await?;
+            } else {
+                // wait for a name to be acquired on the bus before continuing
+                let new_player_stream = self.dbus.receive_name_owner_changed().await?
+                    .filter_map(|e| async move {
+                        let args = e.args().ok()?;
+                        // wait for a new MPRIS player
+                        if let BusName::WellKnown(name) = args.name {
+                            if name.starts_with("org.mpris.MediaPlayer2") {
+                                return Some(name.into_owned());
+                            }
+                        }
+                        None
+                    });
+                pin_mut!(new_player_stream);
+                let new_player: WellKnownName = new_player_stream.next().await.expect("stream will never end");
+
+                debug!("Player {:?} found", new_player);
+                let proxy = MediaPlayerProxy::new(&self.conn, new_player).await?;
+
+                // drop it early so we can write
+                drop(current_player);
+                self.current_player.write()
+                    .expect("can write to current player")
+                    .replace(proxy);
+            }
+        }
+    }
+}
+
+#[test]
+fn test_asd() {
+    env_logger::init();
+    let context = gtk::glib::MainContext::new();
+    context.block_on(async {
+        let controller = MprisController::init().await.unwrap();
+        controller.watch_changes().await.unwrap();
+    });
 }
