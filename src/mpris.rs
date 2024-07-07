c8639caa3df1796d8ca91894a372e51dbf25fb6c
Siddhant Kameswar <115331356+grimsteel@users.noreply.github.com>
2024-07-06 19:31:13 -0500

    mpris stream throttling

--- src/mpris.rs
+++ src/mpris.rs
@@ -1,9 +1,8 @@
-use std::collections::HashMap;
+use std::{collections::HashMap, time::{Duration, Instant}};
 
+use async_io::Timer;
 use zbus::{proxy, Connection, zvariant::Value};
 use futures::{channel::mpsc::Sender, pin_mut, select, stream::StreamExt, SinkExt};
-
-use log::debug;
 
 #[proxy(default_path = "/org/mpris/MediaPlayer2", interface = "org.mpris.MediaPlayer2.Player", gen_blocking = false, default_service = "org.mpris.MediaPlayer2.playerctld")]
 trait MediaPlayer {
@@ -30,7 +29,6 @@ trait PlayerCtlD {
 }
 
 pub struct MprisController<'a> {
-    conn: Connection,
     player_proxy: MediaPlayerProxy<'a>,
     playerctl_proxy: PlayerCtlDProxy<'a>
 }
@@ -57,13 +55,14 @@ pub struct MediaInfo {
     pub state: MediaState
 }
 
+const STREAM_THROTTLE: Duration = Duration::from_secs(1);
+
 impl<'a> MprisController<'a> {
     pub async fn init() -> zbus::Result<Self> {
         let conn = Connection::session().await?;
         let player_proxy = MediaPlayerProxy::new(&conn).await?;
         let playerctl_proxy = PlayerCtlDProxy::new(&conn).await?;
         Ok(Self {
-            conn,
             player_proxy,
             playerctl_proxy
         })
@@ -79,6 +78,10 @@ impl<'a> MprisController<'a> {
             }).fuse();
         pin_mut!(players_stream);
 
+        let _ = tx.send(None).await;
+
+        let mut last_sent = Instant::now();
+
         // wait for at least one player to start before proceeding
         while let Some(false) = players_stream.next().await {}
         
@@ -90,10 +93,15 @@ impl<'a> MprisController<'a> {
         let mut current_media_info = MediaInfo::default();
         let mut players_exist = true;
 
+        let mut need_send = false;
+
+        let mut timer = Timer::interval(STREAM_THROTTLE).fuse();
+
         loop {
             select! {
                 new_players_exist = players_stream.next() => {
                     players_exist = new_players_exist.unwrap_or_default();
+                    need_send = true;
                 },
                 metadata = metadata_stream.next() => {
                     if let Some(metadata) = metadata {
@@ -107,12 +115,14 @@ impl<'a> MprisController<'a> {
                             current_media_info.track = "".into();
                             current_media_info.duration = 0;
                         }
+                        need_send = true;
                     }
                 },
                 pos = position_stream.next() => {
                     if let Some(pos) = pos {
                         let pos = pos.get().await.unwrap_or_default();
                         current_media_info.position = pos.try_into().unwrap_or_default();
+                        need_send = true;
                     }
                 },
                 volume = volume_stream.next() => {
@@ -121,6 +131,7 @@ impl<'a> MprisController<'a> {
                         // add bounds to the volume and cast to u8
                         let volume = (volume * 100f64).max(0f64).min(100f64) as u8;
                         current_media_info.volume = volume;
+                        need_send = true;
                     }
                 },
                 status = playback_status_stream.next() => {
@@ -133,37 +144,29 @@ impl<'a> MprisController<'a> {
                                 _ => MediaState::Stopped
                             }).unwrap_or_default();
                         current_media_info.state = status;
+                        need_send = true;
                     }
-                }
+                },
+                _ = timer.next() => {}
             };
 
-            if tx.send(
-                if players_exist {
-                    Some(current_media_info.clone())
+            // If the last sent was more than 100 ms ago, send a new one
+            if last_sent.elapsed() > STREAM_THROTTLE && need_send {
+                if tx.send(
+                    if players_exist {
+                        Some(current_media_info.clone())
+                    } else {
+                        None
+                    }).await.is_err()
+                {
+                    break
                 } else {
-                    None
-                }).await.is_err()
-            {
-                break
-            };
+                    last_sent = Instant::now();
+                    need_send = false;
+                }
+            }
         }
 
         Ok(())
     }
-}
-
-#[test]
-fn test_asd() {
-    env_logger::init();
-    let context = gtk::glib::MainContext::new();
-    context.block_on(async {
-        let (tx, rx) = futures::channel::mpsc::channel(1);
-        context.spawn_local(async move {
-            let controller = MprisController::init().await.unwrap();
-            controller.watch_changes(tx).await.unwrap();
-        });
-        rx.for_each(|item| async move {
-            println!("item : {item:?}");
-        }).await;
-    });
 }
